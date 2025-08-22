6795183f3979a9384c62ae8897a77036254b8bab
sharvarimalve <sharvari@rsinfotechsys.com>
2025-08-22 09:40:23 +0530

    integration of get video list api

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,12 +1,14 @@
 import Layout from "@/components/layout/Layout";
 import UserManagement from "./UserManagement"; 
 import { CoinManagement } from "./coin/CoinManagement";
+import { VideoManagement } from "./videomange/VideoManagement";
 
 export default function Page() {
   return (
     <Layout>
       <UserManagement />
       <CoinManagement />
+      <VideoManagement/>
     </Layout>
   );
 }
---
+++ app/dashboard/videomange/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../../../components/layout/Layout";
+import { VideoManagement } from "./VideoManagement";
+
+function Page() {
+  return (
+    <Layout>
+      <VideoManagement/>
+    </Layout>
+  );
+}
+
+export default Page;
